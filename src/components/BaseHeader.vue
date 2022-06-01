1fec764faea45bd5953281119da1aae71567182e
AlessP93 <alespiscino@libero.it>
2022-06-01 17:46:40 +0200

    header e main finito

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="header">
+      <div class="container">
+        <div class="header__logo">
+            <img src="../assets/img/dc-logo.png">
+        </div>
+        <nav class="header__nav">
+            <ul>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+            </ul>
+        </nav>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    justify-content: space-between;
+
+    &__logo {
+        padding: .625rem;
+
+        img {
+            width: 80%;
+        }
+    }
+
+
+    &__nav {
+        align-self: center;
+        ul {
+            display: flex;
+            list-style: none;
+            column-gap: 2.5rem
+        }
+        a {
+            text-decoration: none;
+            color: black;
+        }
+    }
+}
+.container {
+     display: flex;
+     justify-content: space-between;
+    }
+</style>
---
+++ src/components/BaseMain.vue
@@ -0,0 +1,23 @@
+<template>
+<main>
+    <div class="container">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus blanditiis neque, aliquid quibusdam voluptas vel nisi a cum id officia exercitationem, accusamus rerum necessitatibus hic eligendi, dolorum cupiditate? Omnis, perspiciatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit voluptatum iusto, assumenda porro consequuntur odit, doloribus laborum eum repudiandae nam numquam blanditiis quidem. Ab a, aut perferendis inventore odio delectus!</p>
+    </div>
+</main>
+</template>
+
+<script>
+export default {
+    name: 'BaseMain',
+}
+</script>
+
+<style>
+    main {
+        background-color: var(--secondary-color);
+        padding: 1.25rem;
+    }
+    p {
+        color: white;
+    }
+</style>
