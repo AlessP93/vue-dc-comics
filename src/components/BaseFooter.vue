1e947d35f315bae6c535cd312bae1912b9dd9465
AlessP93 <alespiscino@libero.it>
2022-06-01 18:21:37 +0200

    iniziato footer

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,49 @@
+<template>
+  <footer class="footer">
+      <div class="footer__top">
+        <div>
+            <img src="../assets/img/buy-comics-digital-comics.png">
+            <div>DIGITAL COMICS</div>
+        </div>
+        <div>
+            <img src="../assets/img/buy-comics-merchandise.png">
+            <div>DIGITAL COMICS</div>
+        </div>
+        <div>
+            <img src="../assets/img/buy-comics-subscriptions.png">
+            <div>DIGITAL COMICS</div>
+        </div>
+        <div>
+            <img src="../assets/img/buy-comics-shop-locator.png">
+            <div>DIGITAL COMICS</div>
+        </div>
+        <div>
+            <img src="../assets/img/buy-comics-digital-comics.png">
+            <div>DIGITAL COMICS</div>
+        </div>
+      </div>
+      <div class="footer__middle"></div>
+      <div class="footer__bottom"></div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'BaseFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+    .footer {
+        background-color: var(--tertiary-color);
+    
+    &__top {
+        display: flex;
+        justify-content: space-around;
+
+        img {
+            width: 30%;
+        }
+    }
+}
+</style>
